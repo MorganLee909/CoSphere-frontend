bfa4296a681ee32b48d6e8e08b9af756caaffb50
Lee Morgan <lee@leemorgan.dev>
2024-08-03 15:49:28 -0400

    Hide/display aside conditionally.

--- src/routes/docs/api/+page.svelte
+++ src/routes/docs/api/+page.svelte
@@ -1,10 +1,43 @@
 <script>
     import "../../../global.css";
-    import Menu from "./Menu.svelte"
+    import Menu from "./Menu.svelte";
+    import User from "./components/User.svelte";
+    import UserExample from "./components/UserExample.svelte";
+
+    let showAside = false;
+    let currentAside = "";
+
+    function displayAside(event){
+        console.log("show aside");
+        showAside = !showAside;
+        currentAside = event.detail.currentAside;
+    }
 </script>
 
-<Menu></Menu>
+<div id="container">
+    <Menu></Menu>
+
+    <main>
+        <User on:displayAside={displayAside}></User>
+    </main>
+
+    {#if showAside} 
+        <aside>
+            {#if currentAside === "userExample"}
+                <UserExample></UserExample>
+            {/if}
+        </aside>
+    {/if}
+</div>
 
 <style>
+    #container{
+        display: flex;
+        background: rgb(20, 20, 20);
+        color: white;
+    }
 
+    main{
+        width: 50vw;
+    }
 </style>
---
+++ src/routes/docs/api/components/UserExample.svelte
@@ -0,0 +1,23 @@
+<pre>
+    <code>
+{'{'}
+    "_id": "507f191e810c19729de860ea",
+    "email": "john.smith@mail.com",
+    "first_name": "John",
+    "last_name": "Smith",
+    "status": "Active",
+    "avatar": "/api/avatar/507f1f77bcf86cd799439011.webp",
+    "default_location": "6592008029c8c3e4dc76256c"
+{'}'}
+    </code>
+</pre>
+
+<style>
+    pre{
+        background: rgb(24, 27, 56);
+        border-radius: 15px;
+        padding: 5px 15px;
+        font-size: 16px;
+        line-height: 26px;
+    }
+</style>
